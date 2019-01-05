2781db767f90f2e6657732a879e25991541a57c4
flamel <flamel90@gmail.com>
2019-01-05 12:59:19 +0100

    testing and ordered csv n stuff

--- sources/population.py
+++ sources/population.py
@@ -2,8 +2,8 @@ import csv
 
 # data from https://www.destatis.de/DE/ZahlenFakten/LaenderRegionen/Regionales/Gemeindeverzeichnis/Administrativ/Aktuell/05Staedte.xlsx?__blob=publicationFile
 
-def get_citites():
-    with open("../data/destatis-staedte.csv", "r") as f:
+def get_citites_population(source="../data/destatis-staedte.csv"):
+    with open(source, "r") as f:
         reader = csv.DictReader(f)
         cities = {
             entry['Stadt']: int(entry['Bevölkerung insgesamt'].replace(" ", ""))
